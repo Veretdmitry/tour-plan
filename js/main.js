c2351cb4693dd0b8fc1942a26eb4fc4cd09c31f0
Dmitry Veretelnikov <veretdmitry2000@mail.ru>
2020-09-23 12:26:28 +0300

    create: add map in hotel section

--- js/main.js
+++ js/main.js
@@ -15,3 +15,21 @@ var mySwiper = new Swiper('.swiper-container', {
     delay: 5000,
   },
 })
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [-8.825719, 115.218629],
+    zoom: 7
+  });
+  myMap.geoObjects.add(new ymaps.Placemark([-8.825719, 115.218629], {
+    balloonContent: '<strong>Hilton Bali Resort</strong>'
+  }))
+  myMap.geoObjects.add(myPlacemark);
+
+  // Открываем балун на карте (без привязки к геообъекту).
+  myMap.balloon.open([-8.825719, 115.218629], "Hilton Bali Resort", {
+    // Опция: не показываем кнопку закрытия.
+    closeButton: true
+  });
+}
