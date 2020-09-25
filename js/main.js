9889451f1c55876a2c62e9486ffae728e0f86138
Dmitry Veretelnikov <veretdmitry2000@mail.ru>
2020-09-25 23:15:49 +0300

    create: review - block and fix: folder's plan

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button--next',
-    prevEl: '.swiper-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
   keyboard: {
     enabled: true,
@@ -15,6 +15,21 @@ var mySwiper = new Swiper('.swiper-container', {
     delay: 5000,
   },
 })
+var reviewSlider = new Swiper('.review-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.review-slider__button--next',
+    prevEl: '.review-slider__button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+})
+
 
 ymaps.ready(init);
 function init() {
@@ -25,6 +40,6 @@ function init() {
   myMap.geoObjects.add(new ymaps.Placemark([52.351250, 4.872511], {
     balloonContent: '<strong>Hilton Amsterdam</strong>'
   }))
-
-
 }
+
+$('.subscribe').parallax({ imageSrc: '../img/subscribe-bg.jpg' });
