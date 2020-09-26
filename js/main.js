732940c8ec90e505226b9c4b3d8e1b86ca19862c
Dmitry Veretelnikov <veretdmitry2000@mail.ru>
2020-09-26 23:34:25 +0300

    fix: tablet of review + delete parallax and made google map

--- js/main.js
+++ js/main.js
@@ -28,25 +28,7 @@ var reviewSlider = new Swiper('.review-slider', {
     enabled: true,
     onlyInViewport: false,
   },
-  autoplay: {
-    delay: 5000,
-  },
-});
-
-
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [52.351250, 4.872511],
-    zoom: 7
-  });
-  myMap.geoObjects.add(new ymaps.Placemark([52.351250, 4.872511], {
-    balloonContent: '<strong>Hilton Amsterdam</strong>'
-  }))
-};
-
-$('.subscribe').parallax({ 
-  imageSrc: 'img/subscribe-bg.jpg',
-  speed: 0.2,
 
 });
+
+
